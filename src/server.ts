2f3356ae58d265420fc9acf74d9ce3490d700752
KevinAlves55 <programador.kevin4773@gmail.com>
2022-09-14 15:39:35 -0300

    feat: Configurando o Prisma

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,34 @@ import express from "express";
 
 const app = express();
 
-app.get("/ads", (req, res) => {
+app.get("/games", (req, res) => {
+
+    return res.json([]);
+
+});
+
+app.post("/ads", (req, res) => {
+
+    return res.status(201).json([]);
+
+});
+
+app.get("/games/:id/ads", (req, res) => {
+
+    const idGame = req.params.id;
+
+    return res.json([
+        { id: 1, name: "Kevin" },
+        { id: 1, name: "Kevin" },
+        { id: 1, name: "Kevin" },
+        { id: 1, name: "Kevin" },
+    ]);
+
+});
+
+app.get("/ads/:id/discord", (req, res) => {
+
+    const idAds = req.params.id;
 
     return res.json([
         { id: 1, name: "Kevin" },
